f4b8766a53fcb3589cba0591466d9193eeb50fac
oscar <oscar.burman005@gmail.com>
2025-03-06 22:11:49 +0100

    Trying to change reverse proxy to resolve MIME error

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ if (NODE_ENV === "production") {
   console.log("Running in production mode...");
   app.use(express.static(path.join(__dirname, "public", "dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "public", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    if (req.accepts("html")) {
+      res.sendFile(path.join(__dirname, "public", "dist", "index.html"));
+    }
   });
 }
 
