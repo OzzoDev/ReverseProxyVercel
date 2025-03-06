b486ae0caca4f049d32b44970cbfa6d3b5add9a3
oscar <oscar.burman005@gmail.com>
2025-03-06 21:51:52 +0100

    Undid vercel.json edit

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -13,12 +13,5 @@ export default function App() {
     })();
   }, []);
 
-  // "builds": [
-  //     {
-  //         "src": "server.js",
-  //         "use": "@vercel/node"
-  //     }
-  // ],
-
   return <div>{data}</div>;
 }
