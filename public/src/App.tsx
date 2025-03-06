f08af948f4ddbe00c7fbfaa8a786fe6e4a232eae
oscar <oscar.burman005@gmail.com>
2025-03-06 22:40:17 +0100

    Removed fetch

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -3,23 +3,23 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [data, setData] = useState<string>("");
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch("https://reverse-proxy-vercel-one.vercel.app/api/data");
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch("https://reverse-proxy-vercel-one.vercel.app/api/data");
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
 
-        const result = await response.json();
-        setData(result.data);
-      } catch (error) {
-        console.error("API fetch failed:", error);
-        setData("Failed to load data.");
-      }
-    })();
-  }, []);
+  //       const result = await response.json();
+  //       setData(result.data);
+  //     } catch (error) {
+  //       console.error("API fetch failed:", error);
+  //       setData("Failed to load data.");
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <div>
